211da8b40ae4cb0db94e5c8192092913eaf554a6
Phuc Le Hong <phuc.le@siliconstack.com.au>
2024-01-16 11:07:25 +0700

    Implement

--- src/Identity/Playground.Identity/Program.cs
+++ src/Identity/Playground.Identity/Program.cs
@@ -1,11 +1,19 @@
+using Playground.Extension;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.ConfigureCors();
 // Add services to the container.
+
+builder.Host.ConfigureLogging();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSerilogMiddleware();
 
 var app = builder.Build();
 
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseSerilogRequestLogging();
+app.UseSerilogMiddleware();
 
 app.UseHttpsRedirection();
 
---
+++ src/Shared/Playground.Extensions/CorsExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Playground.Extension;
+public static class CorsExtension
+{
+    public static void ConfigureCors(this IServiceCollection services)
+    => services.AddCors(options =>
+    {
+        options.AddPolicy("DefaultCors", builder
+            => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+    });
+
+    public static void ConfigureCors(this IServiceCollection services, IConfiguration configuration)
+    {
+        var allowedHosts = configuration.GetSection("AllowedHosts").Get<string[]>() ?? [];
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CustomCors", builder
+                => builder.WithOrigins(allowedHosts)
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+        });
+    }
+}
